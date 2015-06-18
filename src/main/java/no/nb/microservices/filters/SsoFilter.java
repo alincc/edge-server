8b91e164cae4dca300d32caf53609a7d3fe24476
ronnymikalsen <ronny.mikalsen@nb.no>
2015-06-18 11:29:17 +0200

    BT-3225 improved SSO filter

--- src/main/java/no/nb/microservices/filters/SsoFilter.java
+++ src/main/java/no/nb/microservices/filters/SsoFilter.java
@@ -1,11 +1,8 @@
 package no.nb.microservices.filters;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
+import no.nb.commons.web.util.UserUtils;
 
-import org.apache.commons.validator.routines.InetAddressValidator;
 import org.springframework.stereotype.Component;
-import org.springframework.web.util.WebUtils;
 
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
@@ -35,60 +32,13 @@ public class SsoFilter extends ZuulFilter {
     }
 
     private void addClientIpToRequest() {
-        HttpServletRequest request = RequestContext.getCurrentContext()
-                .getRequest();
         RequestContext ctx = RequestContext.getCurrentContext();
-        
-        String clientIp = getClientIp(request);
-        validateClientIp(clientIp);
-
-        ctx.addZuulRequestHeader("X-Original-IP-Fra-Frontend", "" + clientIp);
+        ctx.addZuulRequestHeader(UserUtils.REAL_IP_HEADER, UserUtils.getClientIp());
     }
 
     private void addAmssoToRequest() {
-        HttpServletRequest request = RequestContext.getCurrentContext()
-                .getRequest();
-
         RequestContext ctx = RequestContext.getCurrentContext();
-        String amsso = getAmSso(request);
-        ctx.addZuulRequestHeader("amsso", "" + amsso);
-    }
-
-    private String getAmSso(HttpServletRequest request) {
-        
-        String amsso = request.getHeader("amsso");
-        
-        if (amsso != null) {
-            return amsso;
-        } else {
-            Cookie amssoCookie = WebUtils.getCookie(request, "amsso");
-            if (amssoCookie != null) {
-                return amssoCookie.getValue();
-            }
-        }
-        
-        return null;
-    }
-
-    private void validateClientIp(String clientIp) {
-        if (!InetAddressValidator.getInstance().isValidInet4Address(clientIp)
-                || "127.0.0.1".equals(clientIp)) {
-            throw new SecurityException("localhost or IPv6 is not supported (" + clientIp + ")");
-        }
-    }
-
-    private String getClientIp(HttpServletRequest request) {
-        String clientIp = request.getHeader("X-Original-IP-Fra-Frontend");
-        if (originalIpIsNull(clientIp)) {
-            return request.getRemoteAddr();
-        } else {
-            return clientIp;
-        }
-
-    }
-
-    private boolean originalIpIsNull(String clientIp) {
-        return clientIp == null || clientIp.isEmpty();
+        ctx.addZuulRequestHeader(UserUtils.SSO_HEADER, UserUtils.getSsoToken());
     }
 
     @Override
