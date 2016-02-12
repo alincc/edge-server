85162d04bc92f8934b531bbfc9f45bc832b65746
Andreas Bjørnådal <bjornadal@gmail.com>
2016-02-12 12:04:37 +0100

    Use config server

--- src/main/java/no/nb/microservices/ZuulApplication.java
+++ src/main/java/no/nb/microservices/ZuulApplication.java
@@ -3,6 +3,7 @@ package no.nb.microservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Controller;
 @EnableAutoConfiguration
 @Controller
 @EnableZuulProxy
+@EnableDiscoveryClient
 public class ZuulApplication {
 
     public static void main(String[] args) {
